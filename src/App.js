dcab6dfa4a295ac43d182270b8792ae2ffc327b4
danielkuzminski <dankuzmina@gmail.com>
2023-11-21 13:17:14 +0100

    clear app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Order from './components/Order';
+import Landing from './components/Landing';
+import Hamburger from './components/Hamburger';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hamburger />
+      <Landing />
+      <Order />
     </div>
   );
 }
---
+++ src/components/Hamburger.jsx
@@ -0,0 +1,13 @@
+import './Hamburger.css'
+
+export default function Hamburger() {
+  return (
+    <div className="hamburger-container">
+        <div className='hamburger'>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+        </div>
+    </div>
+  )
+}
